65fb6c0714f2296fbccfc1cfd25ab8ebb6abab46
Devon <djohnjr22@outlook.com>
2024-05-04 16:45:42 -0400

    update authentication

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -4,12 +4,13 @@ const { UnauthenticatedError } = require("../errors");
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new UnauthenticatedError("Authentication invalid");
-  }
-
-  const token = authHeader.split(" ")[1];
   try {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new UnauthenticatedError("Authentication invalid");
+    }
+
+    const token = authHeader.split(" ")[1];
+
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Token Payload:", payload);
     req.user = {
@@ -18,8 +19,7 @@ const auth = async (req, res, next) => {
     };
     next();
   } catch (error) {
-    console.error("JWT Verification Error:", error);
-    throw new UnauthenticatedError("Authentication failed");
+    next(error);
   }
 };
 
